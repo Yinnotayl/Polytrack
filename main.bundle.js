3bd15a6530f121d2d05fb271578cb22120d31fc2
Yinnotayl <yijue20201@gmail.com>
2026-03-05 10:18:41 +0800

    Added new track

--- main.bundle.js
+++ main.bundle.js
@@ -43,8 +43,8 @@ const YP = class {
                 XP(this, zP, "m", GP).call(this, "tracks/speedster.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/CurvePractice.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/spiral_out.track", e),
-                XP(this, zP, "m", GP).call(this, "tracks/7loops.track", e)/*,
-                XP(this, zP, "m", GP).call(this, "tracks/TestSpeedsd.track", e),
+                XP(this, zP, "m", GP).call(this, "tracks/7loops.track", e),
+                XP(this, zP, "m", GP).call(this, "tracks/TestSpeedsd.track", e)/*,
                 XP(this, zP, "m", GP).call(this, "tracks/thelmpossibleSlidePro.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TheTrackQLD2Lap.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/theRollercoaster.track", e)*/
