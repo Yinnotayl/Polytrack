ba577756d44dc6d0846203689f6e80e2b4c64e56
Yinnotayl <yijue20201@gmail.com>
2026-03-06 10:54:58 +0800

    Clean up commented track calls in main.bundle.js
    Remove commented-out calls for slopedCurve.track and Fun.track.

--- main.bundle.js
+++ main.bundle.js
@@ -45,7 +45,8 @@ const YP = class {
                 XP(this, zP, "m", GP).call(this, "tracks/spiral_out.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/7loops.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TestSpeedsd.track", e),
-                XP(this, zP, "m", GP).call(this, "tracks/slopedCurve.track", e)/*,
+                XP(this, zP, "m", GP).call(this, "tracks/slopedCurve.track", e),
+                XP(this, zP, "m", GP).call(this, "tracks/Fun.track", e)/*,
                 XP(this, zP, "m", GP).call(this, "tracks/thelmpossibleSlidePro.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TheTrackQLD2Lap.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/theRollercoaster.track", e)*/
