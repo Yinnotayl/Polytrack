641221bc1e83b6265a6949327030d2a3cf348f0d
Yinnotayl <yijue20201@gmail.com>
2026-03-09 14:47:11 +0800

    Clean up commented-out track calls in main.bundle.js
    Removed commented-out calls for 'Fun.track' and 'FunLoops.track'.

--- main.bundle.js
+++ main.bundle.js
@@ -46,7 +46,8 @@ const YP = class {
                 XP(this, zP, "m", GP).call(this, "tracks/7loops.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TestSpeedsd.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/slopedCurve.track", e),
-                XP(this, zP, "m", GP).call(this, "tracks/Fun.track", e)/*,
+                XP(this, zP, "m", GP).call(this, "tracks/Fun.track", e),
+                XP(this, zP, "m", GP).call(this. "tracks/FunLoops.track", e)/*,
                 XP(this, zP, "m", GP).call(this, "tracks/thelmpossibleSlidePro.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TheTrackQLD2Lap.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/theRollercoaster.track", e)*/
