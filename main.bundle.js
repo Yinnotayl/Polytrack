82dd4eec28199b7580faea4c49a51434bbd332c3
Yinnotayl <yijue20201@gmail.com>
2026-03-10 16:07:49 +0800

    Added new tracks

--- main.bundle.js
+++ main.bundle.js
@@ -59,10 +59,11 @@ const YP = class {
                 XP(this, zP, "m", GP).call(this, "tracks/slopedCurve.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/Fun.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/FunLoops.track", e),
-                XP(this, zP, "m", GP).call(this, "tracks/Hi.track", e)/*,
+                XP(this, zP, "m", GP).call(this, "tracks/Hi.track", e),
+                XP(this, zP, "m", GP).call(this, "tracks/Upsidedown.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/thelmpossibleSlidePro.track", e),
                 XP(this, zP, "m", GP).call(this, "tracks/TheTrackQLD2Lap.track", e),
-                XP(this, zP, "m", GP).call(this, "tracks/theRollercoaster.track", e)*/
+                XP(this, zP, "m", GP).call(this, "tracks/theRollercoaster.track", e)
             ]).then(arr => {
                 qP(this, FP, arr, "f");
             });
